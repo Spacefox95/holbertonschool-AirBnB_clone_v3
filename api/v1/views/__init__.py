d4db9dbed8b980bd8052d0bed44dc8443513497f
benjaminparentt <p.benjamin620@icloud.com>
2024-04-17 13:50:57 +0200

    api test

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Import all view modules here to register them with the blueprint
+from api.v1.views.index import *
