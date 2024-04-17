0f4b6e65110a5e5001ed8122fefae31421837d2b
benjaminparentt <p.benjamin620@icloud.com>
2024-04-17 14:08:05 +0200

    task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors that returns a JSON-formatted 404 status response."""
+    return jsonify({"error": "Not found"}), 404
 
 @app.teardown_appcontext
 def close_storage(exception):
@@ -11,7 +17,6 @@ def close_storage(exception):
     storage.close()
 
 if __name__ == "__main__":
-    import os
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
