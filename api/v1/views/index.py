a18a856ca7692921e9969f8f1c678f7050f7dee3
benjaminparentt <p.benjamin620@icloud.com>
2024-04-17 13:59:43 +0200

    add index?.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,28 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Returns a JSON status response."""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Retrieves the number of each type of object by type."""
+    classes = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+    }
+    stats = {cls: storage.count(obj) for cls, obj in classes.items()}
+    return jsonify(stats)
