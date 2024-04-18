f5e5c3db64341f32430114f3b75035f8941d9d5b
Spacefox95 <nathan.raynal@gmail.com>
2024-04-18 13:25:30 +0200

    ok

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,7 +11,9 @@ from models.state import State
 from api.v1.views import app_views
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'])
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET'],
+                 strict_slashes=False)
 def get_cities(state_id):
     """ Retrieves the list of all City objects of a State """
     state = storage.get(State, state_id)
@@ -21,7 +23,9 @@ def get_cities(state_id):
     return jsonify(cities)
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'])
+@app_views.route('/cities/<city_id>',
+                 methods=['GET'],
+                 strict_slashes=False)
 def get_city(city_id):
     """ Retrieves a City object """
     city = storage.get(City, city_id)
@@ -30,7 +34,9 @@ def get_city(city_id):
     return jsonify(city.to_dict())
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'])
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """ Creates a City in a State """
     state = storage.get(State, state_id)
@@ -47,7 +53,7 @@ def create_city(state_id):
     return jsonify(new_city.to_dict()), 201
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'])
+@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """ Deletes a City object """
     city = storage.get(City, city_id)
@@ -58,7 +64,7 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'])
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     """ Updates a City object """
     city = storage.get(City, city_id)
