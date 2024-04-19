fd77bd600d5b06520b2bcd32467e7e4707108b63
Spacefox95 <nathan.raynal@gmail.com>
2024-04-19 11:11:23 +0200

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,13 @@ Create a RESTFul API
 
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
